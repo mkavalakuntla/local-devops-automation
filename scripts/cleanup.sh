8afa4f6d664b5f945862a692e597df30b7697f8c
devops <devops123@gmail.com>
2026-02-25 16:16:26 +0530

    fix: correct syntax issues and improve script structure

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -9,7 +9,7 @@ set -o pipefail
 # Author	: Mani Kumar
 # Date		: 23/02/2026
 # Script	: Cleanup of Old Backups(Automatic Retention Policy)
-# Purpose 	: To enable retution policy so that script automatically deletes the old backup file from the backup path based on retention period
+# Purpose 	: To enable retention policy so that script automatically deletes the old backup file from the backup path based on retention period
 # Usage		: /home/devops/local-devops-automation/scripts/cleanup.sh
 #
 ########################################################################################
@@ -18,18 +18,9 @@ set -o pipefail
 
 DATE_FORMAT="%F %T"
 
-log(){
-	local LOG_STATUS="$1"
-	local LOG_MESSAGE="$2"
-	local TIME_STAMP="$(date +"$DATE_FORMAT")"
-	local LOG_ENTRY="$TIME_STAMP [ $LOG_STATUS ] $LOG_MESSAGE"
-
-	echo "$LOG_ENTRY" >> "$LOG_PATH/backup_cleanup.log"
-}
-
 # Validate Arguments count 
 
-if [ $# -nq 1 ];then
+if [ $# -ne 2 ];then
 	echo "[ ERROR ] Invalid number of arguments."
 	echo "[ USAGE ] ./cleanup.sh <source_dir> <log_dir>"
 	exit 1
@@ -37,10 +28,29 @@ fi
 
 # Initialize the variables
 
-BACKUPS_PATH="/home/devops/local-devops-automation/backups"
-LOG_PATH="/home/devops/local-devops-automation/logs"
-RETUNTION_PERIOD=7
+BACKUPS_PATH="$1"
+LOG_PATH="$2"
+RETENTION_PERIOD=7
 
-# Write a backups cleanup function based on retention period
 
+# Create a log directory
+
+mkdir -p "$LOG_PATH"
+
+log(){
+        local LOG_STATUS="$1"
+        local LOG_MESSAGE="$2"
+        local TIME_STAMP="$(date +"$DATE_FORMAT")"
+        local LOG_ENTRY="$TIME_STAMP [ $LOG_STATUS ] $LOG_MESSAGE"
+
+        echo "$LOG_ENTRY" >> "$LOG_PATH/backup_cleanup.log"
+}
+
+# Check the backup directory
+
+if [ ! -d "$BACKUPS_PATH" ];then
+
+	log ERROR "Backup directory does not exist: $BACKUPS_PATH"
+	exit 1
+fi
 
