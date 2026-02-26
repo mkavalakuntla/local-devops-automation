f95015fe7640628794887d0050d22ff2ee91b0ff
devops <devops123@gmail.com>
2026-02-26 11:33:34 +0530

    feat: implement medium-level backup cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -20,6 +20,8 @@ DATE_FORMAT="%F %T"
 
 # Validate Arguments count 
 
+echo "[ INFO ] Checking the Arguments are valid or not "
+
 if [ $# -ne 2 ];then
 	echo "[ ERROR ] Invalid number of arguments."
 	echo "[ USAGE ] ./cleanup.sh <source_dir> <log_dir>"
@@ -35,6 +37,8 @@ RETENTION_PERIOD=7
 
 # Create a log directory
 
+echo "[ INFO ] Creating the log directory"
+
 mkdir -p "$LOG_PATH"
 
 log(){
@@ -48,9 +52,38 @@ log(){
 
 # Check the backup directory
 
+log INFO "Checking the backups path is exist or not"
+
 if [ ! -d "$BACKUPS_PATH" ];then
 
 	log ERROR "Backup directory does not exist: $BACKUPS_PATH"
 	exit 1
 fi
+
+# Check the backups more than 7 days and clear them
+
+log INFO "Checking for backups older than $RETENTION_PERIOD days..."
+
+OLD_BACKUPS=$(find "$BACKUPS_PATH" -type f -name "*.tar.gz" -mtime +"$RETENTION_PERIOD")
+
+if [ -z "$OLD_BACKUPS" ];then
+
+	log INFO "No old backup files are found."
+	exit 0
+else
+	log INFO "Old backup files found:"
+ 	echo "$OLD_BACKUPS" >> "$LOG_PATH/backup_cleanup.log"
+
+	log INFO "Deleting old backups files.."
+
+	find "$BACKUPS_PATH" -type f -name "*.tar.gz" -mtime +"$RETENTION_PERIOD" -delete
+
+	log INFO "Old backups are deleted successfully.."
+fi
+
+exit 0
+
+
+
+
 
